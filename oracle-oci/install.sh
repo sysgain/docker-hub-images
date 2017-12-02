9910f5a472131c729fdc50370741c3c19941eed7
rraiatsysgain <rrai@sysgain.com>
2017-12-02 13:30:22 -0800

    Added logic to download terraform remote state url

--- oracle-oci/install.sh
+++ oracle-oci/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -o soln.zip $TEMPLATE_URL
 curl -o bmcs_api_key.pem $PRIVATE_KEY_URL
+curl -o tf_remote_config.tf $TERR_REMOTE_STATE_URL
 v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
 v="$v."
 cp -r $v .
