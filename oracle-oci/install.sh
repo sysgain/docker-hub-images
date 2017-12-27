777ef23b10ad1aaa22f8f9bc3501931e35efd0c1
rraiatsysgain <rrai@sysgain.com>
2017-12-26 18:07:31 -0800

    added terraform init command

--- oracle-oci/install.sh
+++ oracle-oci/install.sh
@@ -5,3 +5,12 @@ curl -o tf_remote_config.tf $TERR_REMOTE_STATE_URL
 v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
 v="$v."
 cp -r $v .
+echo "Initializing terraform..."
+terraform init \
+-backend-config="bucket=$TF_VAR_terraform_remote_bucket" \
+-backend-config="key=$TF_VAR_deployment_auuid/terraform.tfstate" \
+-backend-config="region=$TF_VAR_terraform_remote_bucket_region" \ 
+-backend-config="encrypt=true" \
+-backend-config="access_key=$TF_VAR_aws_access_key" \ 
+-backend-config="secret_key=$TF_VAR_aws_secret_key" \
+-force-copy 
